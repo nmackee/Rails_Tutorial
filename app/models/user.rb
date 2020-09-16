9cf4056660e9a28a111d3083f185af838431a8ed
Mackee <mackee.base@gmail.com>
2020-09-16 13:43:34 +0000

    Implement advanced login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
+  
   before_save { self.email = email.downcase }
   #before_save { email.downcase! }
   validates :name, presence: true, length: { maximum: 50 }
@@ -15,4 +17,51 @@ class User < ApplicationRecord
                               BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
+  
+  #ランダムなトークンを返す
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+  
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+  
+  #渡されたトークンがダイジェストと一致したらtrueを返す
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+  
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
+  
+  #演習問題
+  #渡された文字列のハッシュ値を返す
+  # def self.digest(string)
+  #   cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+  #                           BCrypt::Engine.cost
+  #   BCrypt::Password.create(string, cost: cost)
+  # end
+  
+  # #ランダムなトークンを返す
+  # def self.new_token
+  #   SecureRandom.urlsafe_base64
+  # end
+  
+  # class << self
+  #   #渡された文字列のハッシュ値を返す
+  #   def digest(string)
+  #     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+  #                               BCrypt::Engine.cost
+  #     BCrypt::Password.create(string, cost: cost)
+  #   end
+    
+  #   #ランダムなトークンを返す
+  #   def new_token
+  #     SecureRandom.urlsafe_base64
+  #   end
+  # end
 end
