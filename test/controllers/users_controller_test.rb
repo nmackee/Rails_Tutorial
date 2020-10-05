41ccd9093312f97d881bd4a15b592a12586a377e
Mackee <mackee.base@gmail.com>
2020-10-05 23:49:39 +0000

    Add user following

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -74,4 +74,14 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to root_url
   end
   
+  test "should redirect followinf when not logged in" do
+    get following_user_path(@user)
+    assert_redirected_to login_url
+  end
+  
+  test "should redirect followers when not logged in" do
+    get followers_user_path(@user)
+    assert_redirected_to login_url
+  end
+  
 end
--- test/integration/users_profile_test.rb
+++ test/integration/users_profile_test.rb
@@ -10,6 +10,13 @@ class UsersProfileTest < ActionDispatch::IntegrationTest
     @user = users(:michael)
   end
   
+  test "count relationships" do
+    log_in_as(@user)
+    get root_path
+    assert_match @user.active_relationships.count.to_s, response.body
+    assert_match @user.passive_relationships.count.to_s, response.body
+  end
+  
   test "profile display" do
     get user_path(@user)
     assert_template 'users/show'
@@ -21,5 +28,7 @@ class UsersProfileTest < ActionDispatch::IntegrationTest
     @user.microposts.paginate(page: 1).each do |micropost|
       assert_match micropost.content, response.body
     end
+    assert_match @user.active_relationships.count.to_s, response.body
+    assert_match @user.passive_relationships.count.to_s, response.body
   end
 end
